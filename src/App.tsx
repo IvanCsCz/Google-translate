41e1444d8ba2fad993f551f4b30ada9d5ea188a9
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-09-10 21:13:50 -0600

    useReducer implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import { ReactElement } from 'react'
+import style from './App.module.css'
+import InterchangeIcon from './components/InterchangeIcon'
+import { AUTO_LANGUAGE } from './constants'
+import { useLanguage } from './hooks/useLanguage'
 
 function App (): ReactElement {
+  const {fromLanguage, toLanguage, formText, result, loading, interchangeLanguages, setFormLanguage, setToLanguage, setFormText,setResult} = useLanguage()
+
   return (
-    <>
+    <div className={style.mainWrapper}>
       <h1>Google translate</h1>
-    </>
+
+      <div className={style.languageWrapper}>
+        <div className={style.translateWrapper}>
+          <h2>From</h2>
+          <p>{fromLanguage}</p>
+        </div>
+
+        <button disabled={fromLanguage === AUTO_LANGUAGE} className={style.changeButton} onClick={interchangeLanguages}>
+          <InterchangeIcon />
+        </button>
+
+        <div className={style.translateWrapper}>
+          <h2>To</h2>
+          <p>{toLanguage}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/hooks/useLanguage.tsx
@@ -0,0 +1,86 @@
+import { useReducer } from "react"
+import { AUTO_LANGUAGE } from "../constants"
+import { FromLanguage, Language, LanguageActions, LanguageState } from "../types"
+
+const initialState: LanguageState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  formText: '',
+  result: '',
+  loading: false
+}
+
+const reducer = (state: LanguageState, action: LanguageActions) => {
+  const { type } = action
+
+  if(type === 'INTERCHANGE_LANGUAGES'){
+    if(state.fromLanguage === AUTO_LANGUAGE) return state
+    
+    return {
+      ...state,
+      toLanguage: state.fromLanguage,
+      fromLanguage: state.toLanguage,
+    }
+  }
+
+  if(type === 'SET_FROM_LANGUAGE'){
+    return {
+      ...state,
+      fromLanguage: action.payload,
+    }
+  }
+
+  if(type === 'SET_TO_LANGUAGE'){
+    return {
+      ...state,
+      toLanguage: action.payload,
+    }
+  }
+
+  if(type === 'SET_FROM_TEXT'){
+    return {
+      ...state,
+      loading: true,
+      formText: action.payload,
+      result: ''
+    }
+  }
+
+  if(type === 'SET_RESULT'){
+    return {
+      ...state,
+      loading: false,
+      result: action.payload,
+    }
+  }
+
+  return state
+}
+
+export function useLanguage (){
+  const [{fromLanguage, toLanguage, formText, result, loading}, dispatch] = useReducer(reducer, initialState)
+
+  const interchangeLanguages = () => {
+    dispatch({type: 'INTERCHANGE_LANGUAGES'})
+  }
+
+  const setFormLanguage = (payload: FromLanguage) => {
+    dispatch({type: 'SET_FROM_LANGUAGE', payload })
+  }
+
+  const setToLanguage = (payload: Language) => {
+    dispatch({type: 'SET_TO_LANGUAGE', payload })
+  }
+
+  const setFormText = (payload: string) => {
+    dispatch({type: 'SET_FROM_TEXT', payload })
+  }
+
+  const setResult = (payload: string) => {
+    dispatch({type: 'SET_RESULT', payload })
+  }
+
+  return {
+    fromLanguage, toLanguage, formText, result, loading, interchangeLanguages, setFormLanguage, setToLanguage, setFormText,setResult
+  }
+}
